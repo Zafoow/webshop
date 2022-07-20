561b3f0a7c524b9b8410eb1d51629c5876827e5f
Zafoow <zaferbagcan1994@hotmail.com>
2022-07-20 12:30:23 +0200

    test this brach

--- querries.js
+++ querries.js
@@ -2,37 +2,37 @@ const Product = require("./models").product;
 const User = require("./models").user;
 const Categorie = require("./models").categorie;
 
-async function getProducts(id) {
-  try {
-    const allProducts = await Product.findByPk(id);
-    console.log("Do I get product", allProducts);
-  } catch (e) {
-    console.log(e.message);
-  }
-}
-
-getProducts(2);
-
-async function getUsers() {
-  const allUsers = await Product.findAll();
-  return allUsers.map((product) => product.get({ plain: true }));
-}
-
-// getUsers().then((users) => console.log(users));
-
-async function getCategories() {
-  const allUsers = await Categorie.findAll();
-  return allUsers.map((categorie) => categorie.get({ plain: true }));
-}
-
-// getCategories().then((categories) => console.log(categories));
-
-// async function categoryProducts() {
-//   const catPro = await Categorie.findOne({
-//     where: { title: "Electronics" },
-//     include: [Product],
-//   });
-//   return catPro();
+// async function getProducts(id) {
+//   try {
+//     const allProducts = await Product.findByPk(id);
+//     console.log("Do I get product", allProducts);
+//   } catch (e) {
+//     console.log(e.message);
+//   }
 // }
 
-// // categoryProducts().then((categories) => console.log(categories));
+// getProducts(2);
+
+// async function getUsers() {
+//   const allUsers = await Product.findAll();
+//   return allUsers.map((product) => product.get({ plain: true }));
+// }
+
+// // getUsers().then((users) => console.log(users));
+
+// async function getCategories() {
+//   const allUsers = await Categorie.findAll();
+//   return allUsers.map((categorie) => categorie.get({ plain: true }));
+// }
+
+// // getCategories().then((categories) => console.log(categories));
+
+// // async function categoryProducts() {
+// //   const catPro = await Categorie.findOne({
+// //     where: { title: "Electronics" },
+// //     include: [Product],
+// //   });
+// //   return catPro();
+// // }
+
+// // // categoryProducts().then((categories) => console.log(categories));
